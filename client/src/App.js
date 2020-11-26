722e7b2abc307f0be92b7bf982869380befe1096
anna dodo <anna.dorenskaya@gmail.com>
2020-11-26 19:44:44 +0100

    search, location btn, toolbar start

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import logo from './logo.svg';
-import './App.css';
-import Map from './components/map/Map'
+import React, { Component } from 'react'
+import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
+import Map from "./components/map/Map";
+import Toolbar from "./components/toolbar/Toolbar";
 
-function App() {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+
+      <Switch>
+      <Route exact path="/" component={Map} />
+
+      </Switch>
+         
+      </div>
+    )
+  }
 }
-
-export default App;
